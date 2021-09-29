e75f8f0ee14be78a2ed20ec6b586ee947796f46a
chadiczc <37184548+chadiczc@users.noreply.github.com>
2021-09-29 15:30:52 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3333;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!22')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
+
+
+
+
+
